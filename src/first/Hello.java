0e6fc2ef9f62e7950b2f4a66cc2aeb7073f4f397
dujinyang <root@cntechpower.com>
2019-07-24 23:26:48 +0800

    first commit

---
+++ src/first/Hello.java
@@ -0,0 +1,14 @@
+package first;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World,你好");
+		Scanner in=new Scanner(System.in);
+		System.out.println("echo:"+in.nextLine());
+	}
+
+}
